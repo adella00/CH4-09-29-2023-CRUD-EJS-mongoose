9121b2c4c93799f2935702297d6cc1271ca55418
Adella Desinta <adelladesinta88@gmail.com>
2023-10-02 03:13:46 +0800

    finish crud admin dashboard, add flash message for notification

--- app.js
+++ app.js
@@ -3,19 +3,39 @@
 // THIRD PARTY PACKAGE/MODULE
 const express = require("express")
 const morgan = require("morgan")
+const flash = require("connect-flash")
+const session = require("express-session")
 
 // OUR OWN PACKAGE/MODULE
 const tourRouter = require("./routes/tourRoutes")
 const userRouter = require("./routes/userRoutes")
+const adminRouter = require("./routes/adminRoutes")
 
 const app = express()
 
 // middleware dari express
 app.use(express.json())
 app.use(morgan("dev"))
+app.use(express.urlencoded({ extended: true }))
+
 // biar bisa baca static file
 app.use(express.static(`${__dirname}/public`))
 
+// setting view engine
+app.set("views", __dirname + "/views")
+app.set("view engine", "ejs")
+
+// flash and express session for notification
+app.use(
+  session({
+    secret: "geeksforgeeks",
+    saveUninitialized: true,
+    resave: true,
+  })
+)
+app.use(flash())
+
+// our middleware example
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString()
   console.log(req.requestTime)
@@ -24,5 +44,6 @@ app.use((req, res, next) => {
 
 app.use("/api/v1/tours", tourRouter)
 app.use("/api/v1/users", userRouter)
+app.use("/", adminRouter)
 
 module.exports = app
